635776ff4906d374b5f74030b3db798510d9f98a
AyoubMeftah02 <meftahayoub494@gmail.com>
2026-06-24 11:45:08 +0100

    Calculating Factorial (id = 45488) solution

---
+++ Factorial.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    static int fact(int n) {
+         if (n == 0) {
+             return 1;
+        } else {
+             return n * fact(n - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int n = scanner.nextInt();
+        System.out.print(fact(n));
+
+        scanner.close();
+    }
+}
